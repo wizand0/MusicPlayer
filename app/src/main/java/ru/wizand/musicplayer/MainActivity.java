819a0e3b549509d9f48263067659fce6ae59db76
wizand0 <xabor@yandex.ru>
2024-08-02 17:04:39 +0300

    very simple player

--- app/src/main/java/ru/wizand/musicplayer/MainActivity.java
+++ app/src/main/java/ru/wizand/musicplayer/MainActivity.java
@@ -1,9 +1,14 @@
 package ru.wizand.musicplayer;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.SeekBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +17,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+    ImageView playPauseIcon;
+    SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +32,62 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.stuff);
+
+        playPauseIcon = findViewById(R.id.playIconImageView);
+
+        seekBar = findViewById(R.id.seekBar);
+        seekBar.setMax(mediaPlayer.getDuration());
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean FromUser) {
+                if (FromUser) {
+                    mediaPlayer.seekTo(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                seekBar.setProgress(mediaPlayer.getCurrentPosition());
+            }
+        }, 0, 1000);
     }
 
     public void play(View view) {
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.pause();
+            playPauseIcon.setImageResource(R.drawable.baseline_play_arrow_24);
+        }
+        else {
+            mediaPlayer.start();
+            playPauseIcon.setImageResource(R.drawable.baseline_pause_24);
+        }
     }
 
     public void next(View view) {
+        seekBar.setProgress(mediaPlayer.getDuration());
+        mediaPlayer.seekTo(mediaPlayer.getDuration());
+        mediaPlayer.pause();
+        playPauseIcon.setImageResource(R.drawable.baseline_play_arrow_24);
     }
 
     public void previous(View view) {
+        seekBar.setProgress(0);
+        mediaPlayer.seekTo(0);
+        mediaPlayer.pause();
+        playPauseIcon.setImageResource(R.drawable.baseline_play_arrow_24);
+
     }
 }
